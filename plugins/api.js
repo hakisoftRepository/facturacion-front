a94df1e8a2b6ccac131523849edc58cb0835a8b4
Kobatime <kobaledajona@gmail.com>
2024-02-25 13:52:05 -0500

    add: primer commit

---
+++ plugins/api.js
@@ -0,0 +1,16 @@
+export default function ({ $axios }, inject) {
+  // Create a custom axios instance
+  const api = $axios.create({
+    headers: {
+      common: {
+        Accept: 'text/plain, */*'
+      }
+    }
+  })
+let url ='https://api-demofacturacion.hakisoft.com/'
+  api.url = url
+  api.setBaseURL(url)
+
+  // Inject to context as $api
+  inject('api', api)
+}
