75ca552c9cadc60018cb8fb76bf9379222564b61
Kobatime <kobaledajona@gmail.com>
2024-02-25 16:40:01 -0500

    add: cambios en dist

--- dist/_nuxt/static/1708893439/usuarios/usuario/nuevo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708893439",layout:"default",error:a,serverRendered:true,routePath:"\u002Fusuarios\u002Fusuario\u002Fnuevo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
