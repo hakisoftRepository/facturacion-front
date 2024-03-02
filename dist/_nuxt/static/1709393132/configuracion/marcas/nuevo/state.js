7ec7a6c0fad34cbe611cea76784edb80f7a43e1d
Kobatime <kobaledajona@gmail.com>
2024-03-02 10:26:04 -0500

    add: cambios para multibellos

---
+++ dist/_nuxt/static/1709393132/configuracion/marcas/nuevo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1709393132",layout:"default",error:a,serverRendered:true,routePath:"\u002Fconfiguracion\u002Fmarcas\u002Fnuevo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
